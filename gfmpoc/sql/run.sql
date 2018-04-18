7047fbb58c1989a144e5159a5d5eae0829c529f3
Dan Maxim <dan.maxim@cegeka.com>
2018-04-18 18:20:42 +0300

    Dan
    Signed-off-by: Dan Maxim <dan.maxim@cegeka.com>

---
+++ gfmpoc/sql/run.sql
@@ -0,0 +1,12 @@
+-- DROP DATABASE gfmpoc;
+-- CREATE DATABASE gfmpoc;
+-- USE gfmpoc;
+
+source tables/Park.sql
+source tables/Au.sql
+source tables/Booking.sql
+
+source data/Park.sql
+source data/Au.sql
+source data/Booking.sql
+
--- gfmpoc/sql/tables/run.sql
+++
@@ -1,8 +0,0 @@
---DROP DATABASE gfmpoc;
-CREATE DATABASE gfmpoc;
-USE gfmpoc;
-
-
-source sql/tables/Park.sql
-source sql/tables/Au.sql
-source sql/tables/Booking.sql
